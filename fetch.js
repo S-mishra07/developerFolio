356cfe45a1fd49dcf31170bf0c7e8f4155fe2b6d
S-mishra07 <sm5423@ic.ac.uk>
2025-02-03 16:51:48 +0000

    removed medium

--- fetch.js
+++ fetch.js
@@ -1,4 +1,4 @@
-fs = require("fs");
+const fs = require("fs");
 const https = require("https");
 process = require("process");
 require("dotenv").config();
@@ -6,16 +6,21 @@ require("dotenv").config();
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
 const USE_GITHUB_DATA = process.env.USE_GITHUB_DATA;
-const MEDIUM_USERNAME = process.env.MEDIUM_USERNAME;
+const USE_MEDIUM_DATA = process.env.USE_MEDIUM_DATA;  // Ensure you have this set in your .env if needed
+
+if (USE_MEDIUM_DATA === "true") {
+    // Add your Medium request code here
+} else {
+    console.log('Medium data fetching is disabled');
+}
 
 const ERR = {
   noUserName:
     "Github Username was found to be undefined. Please set all relevant environment variables.",
   requestFailed:
     "The request to GitHub didn't succeed. Check if GitHub token in your .env file is correct.",
-  requestFailedMedium:
-    "The request to Medium didn't succeed. Check if Medium username in your .env file is correct."
 };
+
 if (USE_GITHUB_DATA === "true") {
   if (GITHUB_USERNAME === undefined) {
     throw new Error(ERR.noUserName);
@@ -56,6 +61,7 @@ if (USE_GITHUB_DATA === "true") {
 }
 `
   });
+
   const default_options = {
     hostname: "api.github.com",
     path: "/graphql",
@@ -91,40 +97,5 @@ if (USE_GITHUB_DATA === "true") {
   });
 
   req.write(data);
-  req.end();
-}
-
-if (MEDIUM_USERNAME !== undefined) {
-  console.log(`Fetching Medium blogs data for ${MEDIUM_USERNAME}`);
-  const options = {
-    hostname: "api.rss2json.com",
-    path: `/v1/api.json?rss_url=https://medium.com/feed/@${MEDIUM_USERNAME}`,
-    port: 443,
-    method: "GET"
-  };
-
-  const req = https.request(options, res => {
-    let mediumData = "";
-
-    console.log(`statusCode: ${res.statusCode}`);
-    if (res.statusCode !== 200) {
-      throw new Error(ERR.requestMediumFailed);
-    }
-
-    res.on("data", d => {
-      mediumData += d;
-    });
-    res.on("end", () => {
-      fs.writeFile("./public/blogs.json", mediumData, function (err) {
-        if (err) return console.log(err);
-        console.log("saved file to public/blogs.json");
-      });
-    });
-  });
-
-  req.on("error", error => {
-    throw error;
-  });
-
   req.end();
 }
